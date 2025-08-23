25988209b000fac4dfd05d505a141e684cd86a7c
GitOmonwale <inesagbozo@gmail.com>
2025-08-23 01:55:00 +0100

    refactor: hero enhanced

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,19 +5,19 @@ import Image from 'next/image'
 const Hero = () => {
     return (
         <section className="relative z-10 py-10">
-            <div className="lg:py-16 py-8 flex flex-col items-center justify-between gap-8">
+            <div className="lg:py-16 md:py-8 py-4 flex flex-col items-center justify-between gap-8">
                 <div className="text-center sm:max-w-4xl max-w-full mx-auto flex flex-col gap-6 items-center justify-between">
-                    <div className="mb-12 max-w-96 mx-auto">
+                    <div className="lg:mb-12 mb-6 max-w-96 mx-auto">
                         <div className="border border-white/20 rounded-full px-6 py-3">
                             <span className="text-white/90 text-sm font-medium">We just raised $20M in Series B. Learn more</span>
                         </div>
                     </div>
-                    <h1 className="text-4xl md:text-6xl lg:text-7xl font-base mb-6 bg-gradient-to-r from-[#dedde2] to-[#80739e] bg-clip-text text-transparent">
+                    <h1 className="text-4xl md:text-6xl lg:text-7xl font-base lg:mb-6 mb-0 bg-gradient-to-r from-[#dedde2] to-[#80739e] bg-clip-text text-transparent">
                         Modern analytics
                         <br />
                         for the modern world
                     </h1>
-                    <p className="text-lg md:text-xl text-light-gray font-base mb-8 max-w-2xl mx-auto">
+                    <p className="text-lg md:text-xl text-light-gray font-base lg:mb-8 mb-0 max-w-2xl mx-auto">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.
                     </p>
                     <div className="hidden md:flex items-center space-x-4">
