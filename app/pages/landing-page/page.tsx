aa0bd99392f92881c994b32fe0f09452bb6ef691
GitOmonwale <inesagbozo@gmail.com>
2025-08-23 02:06:39 +0100

    refactor: hero enhanced

--- app/pages/landing-page/page.tsx
+++ app/pages/landing-page/page.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 import CTA from "../../components/CTA";
 export default function LandingHome() {
   return (
-    <div className="bg-deep-violet">
+    <div className="bg-indigo-black">
       <Image src="/images/decoration-img.png" alt="Hero" width={1000} height={500}
-        className="absolute top-20 left- right-0 object-cover w-full"
+        className="absolute top-10 left-0 right-0 object-cover w-full"
       />
       <main className="max-w-7xl mx-auto px-5 relative z-10">
         <Hero />
