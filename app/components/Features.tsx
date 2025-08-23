7f6985e70abce24e257e147dc50301fce96d229f
GitOmonwale <inesagbozo@gmail.com>
2025-08-23 01:34:51 +0100

    refactor: features section enhanced

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -8,7 +8,7 @@ import { GoCodeSquare } from 'react-icons/go'
 
 const Features = () => {
     return (
-        <section className="lg:py-16 py-12 px-8 flex flex-col items-center justify-between gap-8">
+        <section className="lg:py-16 py-12 md:px-8 px-0 flex flex-col items-center justify-between gap-8">
             <div className='flex flex-col items-center mb-8 relative'>
                 <h2 className="text-3xl md:text-4xl lg:text-5xl font-base mb-6 max-w-96 text-center">
                     Features that work for your future
